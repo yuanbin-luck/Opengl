28518f185ca362fe9f0feb30197ad6df61067328
yuanbin <yuanbin@wajitech.net>
2024-09-25 10:31:15 +0800

    创建版本

---
+++ coreOpengl.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "./glad/include/glad.h"
+#include "./glfw3.4/include/glfw3.h"
+#include "./glm/include/glm.hpp"
+
+#include "./glm/include/gtc/matrix_transform.hpp"
+#include "./glm/include/ext/matrix_clip_space.hpp"
+#include "./glm/include/gtc/type_ptr.hpp"
+
+
+#if _DEBUG
+
+#pragma comment(lib, "C:/Tools/Opengl/glad/lib/gladd.lib")
+#pragma comment(lib,  "C:/Tools/Opengl/glfw3.4/lib/glfw3d.lib")
+#pragma comment(lib,  "C:/Tools/Opengl/glm/lib/glmd.lib")
+#else
+
+#pragma comment(lib, "C:/Tools/Opengl/glad/lib/glad.lib")
+#pragma comment(lib,  "C:/Tools/Opengl/glfw3.4/lib/glfw3.lib")
+#pragma comment(lib,  "C:/Tools/Opengl/glm/lib/glm.lib")
+
+
+#endif
